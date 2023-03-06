5501d86b0963379632a830e923e6ea14fa55717c
Angelo Stewart <sticktology@gmail.com>
2023-03-06 16:19:03 -0500

    added finance models & formatting interface

--- public/app.js
+++ public/app.js
@@ -1,48 +1,23 @@
-"use strict";
-// classes
-// class Invoice {
-//     readonly client: string;
-//     private details: string;
-//     public amount: number;
-//     constructor(c: string, d: string, a: number) {
-//         this.client = c;
-//         this.details = d;
-//         this.amount = a;
-//     }
-//     format() {
-//         return `${this.client} owes $${this.amount} for ${this.details}`
-//     }
-// }
-// shorthand constructor (requires modifier on each attribute ex. readonly/private/public)
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
-const invOne = new Invoice("GKV", "web design", 2000);
-const invTwo = new Invoice("Best Buy", "loss prevention", 350);
-let invoices = [];
-invoices.push(invOne, invTwo);
-invOne.amount = 38934;
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.format());
-});
-const anchor = document.querySelector("a");
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+import { Invoice } from "./models/Invoice.js";
+import { Payment } from "./models/Payment.js";
+import { ListTemplate } from "./models/ListTemplate.js";
 const form = document.querySelector(".new-item-form");
-console.group(form);
 // inputs
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,31 @@
-// classes
-// class Invoice {
-//     readonly client: string;
-//     private details: string;
-//     public amount: number;
-
-//     constructor(c: string, d: string, a: number) {
-//         this.client = c;
-//         this.details = d;
-//         this.amount = a;
-//     }
-
-//     format() {
-//         return `${this.client} owes $${this.amount} for ${this.details}`
-//     }
-// }
-
-// shorthand constructor (requires modifier on each attribute ex. readonly/private/public)
-class Invoice {
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
-
-const invOne = new Invoice("GKV", "web design", 2000);
-const invTwo = new Invoice("Best Buy", "loss prevention", 350);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne, invTwo);
-invOne.amount = 38934;
-
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.format());
-});
-
-const anchor = document.querySelector("a")!;
-
-// console.log(anchor.href);
-
-// const form = document.querySelector('form')!;
+import { Invoice } from "./models/Invoice.js";
+import { Payment } from "./models/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./models/ListTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-console.group(form);
+
 // inputs
 const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+  }
+
+  list.render(doc, type.value, 'end');
+
+  
 });
